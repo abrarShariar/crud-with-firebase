27bf2a33bc767d04436caa904dec4ce25b1b7913
abrarShariar <shariarabrar@gmail.com>
2016-09-07 03:55:38 +0600

    read data script completed

---
+++ src/read.js
@@ -0,0 +1,16 @@
+/*
+  READ operations using firebase
+*/
+const quoteBox = document.getElementById('quoteBox');
+//sync data (JSON) chnages (callback triggers every time data changes)
+userQuoteDB.on('value',snap => {
+    var data =  snap.val();
+    quoteBox.innerText = JSON.stringify(snap.val(),null,3);
+    console.log(data);
+    console.log(snap.child('abrar').val());
+    //retrive data as NAME : QUOTE
+    for(var item in data){
+      var info = snap.child(item).val();
+      console.log(item + " : " + info.quote);
+    }
+});
