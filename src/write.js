131176b289e44c95da8f434e55f1b5a3bcbcff12
abrarShariar <shariarabrar@gmail.com>
2016-09-08 20:13:24 +0600

    write modified

--- src/write.js
+++ src/write.js
@@ -8,7 +8,7 @@ var username = "";
 function formSubmit(){
   //GET input fields and clean
       var quoteInput = document.forms['quoteForm']['quote'].value
-      quoteInput = quoteInput.replace(/[^a-z0-9]/gi,"");
+      quoteInput = quoteInput.replace(/[^a-z0-9]/gi," ");
       var nameInput = document.forms['quoteForm']['username'].value;
       nameInput = nameInput.replace(/[^a-z0-9]/gi,"");
 
