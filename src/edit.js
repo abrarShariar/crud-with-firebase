be0a00e78ab9c2de782c493d7c856dd1b1e64bff
abrarShariar <shariarabrar@gmail.com>
2016-09-08 20:12:07 +0600

    EDIT script complete

--- src/edit.js
+++ src/edit.js
@@ -1,6 +1,7 @@
 const quoteDB = firebase.database().ref().child('user_quotes');
 const nameInput = document.getElementById('editName');
-const quoteInput = document.getElementById('editInput');
+const quoteInput = document.getElementById('newQuote');
+var key = "";
 
 window.onload = function(){
     if(window.location.search.indexOf("name=") > -1){
@@ -14,36 +15,30 @@ window.onload = function(){
 function setEditForm(param){
     //clean param
     var index = param.indexOf('=');
-    var key = param.substring(index+1);
+    key = param.substring(index+1);
     //clean uri
     key = key.replace(/[^a-z0-9]/gi,"");
     //set name in form
     nameInput.value = key;
     //get child of key
-    var quote = userQuoteDB.child(key);
-    console.log(quote);
+    firebase.database().ref('/user_quotes/' + key).once('value').then(function(snap){
+        //console.log(snap.val().quote);
+        quoteInput.value = snap.val().quote;
+    });
 }
 
+//event handler to onsubmit event of update form
+function updateFormSubmit(){
+      var editName = document.forms['updateForm']['editName'].value;
+      editName = editName.replace(/[^a-z0-9]/gi,"");
+      var editQuote = document.forms['updateForm']['newQuote'].value;
+      editQuote = editQuote.replace(/[^a-z0-9]/gi," ");
 
-
-
-
-/*
-//update existing data in firebase
-var key = "abrar";
-var newQuote = "I love RockStar";
-//prepare data
-var data = {
-    name : key,
-    quote : newQuote,
-  };
-
-var updates = {};
-updates['/user_quotes/' + data.name + '/quote/'] = data.quote;
-database.update(updates);
-
-//log changes (debug it)
- userQuoteDB.child('abrar').on('value',snap => {
-         console.log(snap.val().quote);
-});
-*/
+      //remove previous data
+      quoteDB.child(key).remove();
+      //update to firebase
+      var updates = {};
+      updates['/user_quotes/' + editName + '/quote/'] = editQuote;
+      firebase.database().ref().update(updates);
+      location.assign("/#read");
+}
